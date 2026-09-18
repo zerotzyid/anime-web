8dac156f326124d416f207bd1c35feb49900e77b
zerotzyid <zerotzyid@users.noreply.github.com>
2026-09-18 09:25:01 +0000

    fix: correctly set upstream API URL to prevent recursive loop on Vercel

--- config.js
+++ config.js
@@ -1,6 +1,4 @@
 module.exports = {
-  API_BASE_URL: process.env.VERCEL_ENV === 'production'
-    ? 'https://nime-anime-streaming.vercel.app' // Ganti dengan domain Vercel Anda
-    : 'http://localhost:3474',
+  API_BASE_URL: 'https://nime-ku-nu.vercel.app',
   BERKASDRIVE_BASE_URL: 'https://dl.berkasdrive.com',
 };
